11aeb56639ad379978d33c0460233213eabb4bef
dd44it <12nelson13@gmail.com>
2022-10-04 19:05:20 +0300

    feat: add methods getData and showData

--- index.js
+++ index.js
@@ -12,6 +12,24 @@ class Creature {
       this.species = species;
       this.age = age;
       this.gender = gender;
+      this.achievement = [];
+   }
+   getData(){
+      return ['name', 'species', 'age', 'gender'];
+   }
+   showData(){
+      return this.getData().map(data => `${data}: <b>${this[data] }</b>`);
+   }
+   introduce(){
+      return `Hello my name is ${this.name} and I'm ${this.age} years old.`;
+   }
+   addAchievement(achievements){
+      for(let achieve of achievements){
+         this.achievement.push(achieve);
+      }
+   }
+   showAchievement(){
+      return this.achievement.length ? `Achievement, person by ${this.name}: ${this.achievement.join('. ')}` : `This person ${this.name} doesn't exist any achievements`
    }
 }
 
@@ -21,13 +39,12 @@ class Human extends Creature {
       this.leg = leg;
       this.nationality = nationality;
       this.country = country;
-      this.achievement = [];
-   }
-   introduce(){
-      return `Hello my name is ${this.name} and I'm ${this.age} years old.`;
    }
    showCountryInfo(){
       return `I from ${this.country} and I'm ${this.nationality}`;
+   }
+   getData(){
+      return super.getData().concat('leg', 'nationality', 'country')
    }
 }
 
@@ -42,8 +59,17 @@ class Women extends Human {
    }
 }
 
-const person1 = new Man('Stepan', 33,'Ukrainian', 'Ukraine')
-const person2 = new Women('Hanna', 26,'Ukrainian', 'Ukraine')
+const person1 = new Man('Stepan', 33,'Ukrainian', 'Ukraine', );
+const person2 = new Women('Hanna', 26,'Ukrainian', 'Ukraine', );
+const person3 = new Man('Bill', 42, 'British', 'Great Britain', );
 
-console.log(person1)
-console.log(person2)
+person3.addAchievement(['Initiated a company-sponsored mentor program to help 15 at-risk teenagers in the area', 'Increased revenue by 10% using data analysis best practices'])
+
+const listPerson = [person1, person2, person3 ]
+
+listPerson.forEach(person => {
+   print(`${person.showData()} `)
+   print(`${person.showCountryInfo()} `)
+   print(`${person.showAchievement()}  <hr>`)
+})
+
